f097301a0e8fa8dc9b50ee5b89d4e036908e34ac
Muhammad Morgan <muhammadmorgan589@gmail.com>
2026-02-19 01:33:35 +0200

    serve the static assets from the frontend

--- app.js
+++ app.js
@@ -3,17 +3,11 @@ require('express-async-errors');
 
 // extra security packages
 const helmet = require('helmet');
-const cors = require('cors');
 const xss = require('xss-clean');
-const rateLimiter = require('express-rate-limit');
-
-// Swagger
-const swaggerUI = require('swagger-ui-express');
-const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./swagger.yaml');
 
 const express = require('express');
 const app = express();
+const path = require("path");
 
 const connectDB = require('./db/connect');
 const authenticateUser = require('./middleware/authentication');
@@ -24,26 +18,21 @@ const jobsRouter = require('./routes/jobs');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
-app.set('trust proxy', 1);
-app.use(
-  rateLimiter({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
-  })
-);
+// adding middleware to serve the static assets from the client.
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+
 app.use(express.json());
 app.use(helmet());
-app.use(cors());
 app.use(xss());
-
-app.get('/', (req, res) => {
-  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
-});
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
+
+// serving the frontend index.html for all routes that aren't part of the api.
+app.get('*', function (req, res) {
+  res.sendFile(path.resolve(__dirname, "./client/build", "index"))
+})
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -52,7 +41,7 @@ const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI);
+    await connectDB(process.env.MONGODB_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
